43c35466c69573d072cf7cae5c3334487b35601e
HEX <badrulnisakhan@icloud.com>
2026-03-26 19:18:07 +0500

    understanding of strlen
    through this you can understand input with spaces in cstring
    use of strlen

---
+++ use of strlen in cstring and how do we store it.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<string>
+#include<cstring>
+
+using namespace std;
+int main()
+{
+
+	char name[20];
+	cout << "Enter a UserName: ";
+	cin.getline (name,20); // it prints space too
+
+	int length = strlen(name); // store input length
+
+	if (length < 5) {
+		cout << "Too short" << endl;
+	}
+	else if (length > 15) {
+		cout << "Too long " << endl;
+	}
+	else {
+		cout << "UserName is accepted " << endl;
+	}
+	
+	}
